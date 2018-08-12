cf437f52ef3567c1ac8a0184225981fa5e8f6f92
Kevin Lam <lam.kevin86@gmail.com>
2018-08-12 13:17:15 -0700

    Updates how apps are added to server canvas

--- client/src/components/AvailableApps.jsx
+++ client/src/components/AvailableApps.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const AvailableApps = ({name, add, destroy}) => (
+const AvailableApps = ({name, addApp, destroy}) => (
   <div className={`available available__${name}`}>
     <p className={name}>
       {name[0].toUpperCase() + name.slice(1)} 
-      <span onClick={() => add(name)}>+</span> 
+      <span onClick={() => addApp(name)}>+</span> 
       <span onClick={() => destroy(name)}>-</span>
     </p>
   </div>
